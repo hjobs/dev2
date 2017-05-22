1854fe8b92aab35be30e440f23c97b4ad5d3cace
Edmund <edmundtfy@gmail.com>
2017-05-22 18:01:09 +0800

    get route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   match "/uploadtest" => "upload#upload", via: :post
+  get "/uploadtest" => "upload#upload"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
